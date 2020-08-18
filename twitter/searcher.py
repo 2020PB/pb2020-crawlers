65d41b80d6de92309223fdf4c496813441353935
Evan Marcey <emarcey@cbinsights.com>
2020-08-18 14:59:10 -0400

    Streamlining media. Fixing query

--- twitter/searcher.py
+++ twitter/searcher.py
@@ -31,7 +31,6 @@ def run_twitter_searches(since_id: int, queries: List[str], job_mode: str) -> in
     if not since_id:
         since_id = get_since_id_from_file()
     api = build_tweepy_api()
-    # tweets = []
     total_returned_tweets = 0
     processed_id_tweets = set()
     max_processed_id = 0
@@ -39,6 +38,7 @@ def run_twitter_searches(since_id: int, queries: List[str], job_mode: str) -> in
     for query in queries:
         logger.info(f"Querying: {query}")
         cursor = query_twitter(api, query, since_id)
+        query_submissions = []
         for resp in cursor:
             statsd.increment(f"twitter.read_post", 1, tags=[f"job_mode:{job_mode}", f"query:{query}"])
             total_returned_tweets += 1
@@ -47,15 +47,13 @@ def run_twitter_searches(since_id: int, queries: List[str], job_mode: str) -> in
             if id_tweet > max_processed_id:
                 max_processed_id = id_tweet
                 max_processed_time_stamp = tweet["created_at"]
-                # TODO: should we keep updating this file, so it is up to date if the job quits unexpectedly?
-                # pro: can pick up where we left off and avoid duplicates
-                # con: may miss hits from searches that were not yet performed
-                # log_last_processed_id(max_processed_id, max_processed_time_stamp)
             # submissions is a list so we can handle single tweet with multiple media objects
             submissions, processed_id_tweets = convert_tweet(tweet, processed_id_tweets)
             if not submissions:
                 continue
-            bulk_upload_submissions(submissions, TWITTER_LARAVEL_API_KEY, READER_MODE)
+            query_submissions.extend(submissions)
+
+        bulk_upload_submissions(query_submissions, TWITTER_LARAVEL_API_KEY, READER_MODE)
     logger.info(f"total_returned_tweets {total_returned_tweets}")
     if max_processed_id > 0:
         log_last_processed_id(max_processed_id, max_processed_time_stamp)
@@ -69,11 +67,9 @@ def build_tweepy_api():
 
 
 def query_twitter(api, query: str, since_id: int):
-    # TODO: include result_type? (mixed, recent, popular) probably want recent once batching is set up
-    # TODO: why wasn't count working
     return tweepy.Cursor(
         api.search,
-        q=f"{query} filter:videos -is:retweet",
+        q=f"{query} filter:videos -filter:retweets",
         lang="en",
         since_id=since_id,
         include_entities=True,
@@ -84,14 +80,11 @@ def query_twitter(api, query: str, since_id: int):
 def convert_tweet(tweet: Dict[str, Any], processed_id_tweets: Set[int]) -> Tuple[List[RawSubmission], Set[int]]:
     submissions = []
     # TODO: add "monetizable" logger. like, is somebody profiting off this shit?
-    if not is_tweet_relevant(tweet):
-        return None, processed_id_tweets
     if "retweeted_status" in tweet:
         tweet = tweet["retweeted_status"]
     id_tweet = tweet["id"]
     if id_tweet in processed_id_tweets:
         return None, processed_id_tweets
-    logger.info("Found a tweet to add!")
     processed_id_tweets.add(id_tweet)
 
     converted_date = twitter_created_at_to_utc(tweet["created_at"])
@@ -101,47 +94,25 @@ def convert_tweet(tweet: Dict[str, Any], processed_id_tweets: Set[int]) -> Tuple
     elif tweet["user"].get("location"):
         submission_body = f"User Location: {tweet['user']['location']}"
 
-    for media in tweet["extended_entities"]["media"]:
-        if media["type"] != "video":
-            continue
-        logger.info(f"found video for tweet: {tweet['text']}")
-        media_url = get_media_url_from_variants(media["video_info"]["variants"])
-        if not media_url:
-            continue
-
-        submission = RawSubmission(
-            data_source=DataSource.twitter,
-            id_source=id_tweet,
-            submission_title=tweet["text"],
-            submission_datetime_utc=converted_date,
-            submission_community="",  # TODO: is this ok? I
-            submission_url=media["expanded_url"],
-            submission_media_url=media_url,
-            submission_body=submission_body,
-            id_submitter=tweet["user"]["id"],
-        )
-        submissions.append(submission)
+    media_url = f'https://twitter.com/{tweet["user"]["id"]}/status/{id_tweet}'
+    submission = RawSubmission(
+        data_source=DataSource.twitter,
+        id_source=id_tweet,
+        submission_title=tweet["text"],
+        submission_datetime_utc=converted_date,
+        submission_community="",  # TODO: is this ok? I
+        submission_url=media_url,
+        submission_media_url=media_url,
+        submission_body=submission_body,
+        id_submitter=tweet["user"]["screen_name"],
+    )
+    submissions.append(submission)
     return submissions, processed_id_tweets
-
-
-def is_tweet_relevant(tweet: Dict[str, Any]):
-    media = tweet.get("extended_entities", {}).get("media")
-    if media:
-        return True
-    return False
 
 
 def twitter_created_at_to_utc(created_at: str):
     # created at format: "Mon Aug 06 19:28:16 +0000 2018",
     return datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")
-
-
-def get_media_url_from_variants(variants: List[Dict[str, str]]):
-    for variant in variants:
-        if variant["content_type"] == "video/mp4":
-            # TODO: multiple bitrates are available, does it matter which?
-            return variant["url"]
-    return None
 
 
 # TODO: hook up to s3 bucket
